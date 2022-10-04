d38c545af85077bfb8897b93d7918e100b0940d9
charchil raj <114642639+Rajcharchil@users.noreply.github.com>
2022-10-04 23:11:02 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,127 @@
+# > replacing the space
+#st = "this is a1  string with double    spaces "
+#st = st.replace("    ","   ")
+#print(st)
+
+##from random import randint
+#a = (1,2,3,3,5)
+#print (randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep='')
+
+
+##print 32 keywords 
+#import  keyword 
+#print(keyword.kwlist)
+
+
+##RANGE
+#sequence1 = range(85,2,-8)
+#for x in sequence1:
+ #   print(x)
+
+
+##list
+
+# =[46,42,'amartya','charchil',34]
+#l[3] = 'kaushik'
+#print(l)
+
+#range
+
+#r = range(10)
+
+#for x in r:
+# #   print(x)
+#a = "32"
+#b = "84"
+#print(a != b)
+#print (type(a))
+#print(type(b))
+
+#r = range(0,10)
+#for i in r:
+ #   print(i)
+
+
+
+##slicing
+#ram = "charchil"
+#print(ram[-4:-1])
+
+##slicing with skip value
+#word = "amazing"
+#print(word [1:6:2])
+
+##string
+##name = "ram is a good boy"
+#print("he said," + name)
+
+##length of string 
+#fruit = "mango"
+#len1 = len(fruit)
+#print("mango is a",len1,"letters")
+
+
+## string as an array:
+#pie = "applepie"
+#print(pie[:5])
+#print(pie[6])
+
+
+
+
+##loop through a string 
+#alphabets = "ABCDEF"
+#for i in alphabets:
+ #   print(i)
+
+
+##string method / upper case
+#str1 = "AbcDEfghIJ"
+#print(str1.upper())
+
+##lower case 
+#str1 = "AbcDEfghIJ"
+#print(str1.lower())
+
+##strip 
+#str2 = "     silver spoon       "
+#print(str2.strip())
+
+##split
+#str2 = "Silver Spoon"
+#print(str2.split(" ")) 
+
+
+##capitalize()
+#str1 = "hello"
+#capStr1 = str1.capitalize()
+#print(capStr1)
+#str2 = "Hello WorlD"
+#capStr2 = str2.capitalize()
+#print(capStr2)
+
+
+##center()
+#str1 = "Welcome to the Console!!!"
+#print(str1.center(5000))
+
+##padding character in #center
+#str1 = "welcom!!!"
+#print(str1.center(100,"_"))
+
+##count
+#str2 = "Abracadabra"
+#print(str2.count("a"))
+
+##endswitch
+#str1 = "Welcome to the Console !!!"
+#print(str1.endswith("!!!"))
+
+#str1 = "Welcome to the Console !!!"
+#print(str1.endswith("o ", 4, 11))
+
+#find()
+#str1 = "He's name is Dan. He is an honest man."
+#print(str1.find("."))
+
+
