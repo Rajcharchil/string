3b82083c832c9a20bb0ff808e600746d302b0d08
charchil raj <114642639+Rajcharchil@users.noreply.github.com>
2022-10-18 10:02:26 +0530

    Add files via upload

---
+++ python_class_16_10.py
@@ -0,0 +1,126 @@
+#from random import randint
+#a = (1,2,3,3,5)
+#print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep='')
+#
+#
+###
+#import random
+#print(random.randint(1,5))
+###
+#
+#a = "sidhu launde"
+#print(a[:-4])
+####
+#age=18
+#hey="my name is charchil and age is {}"
+#print(hey.format(age))
+#
+#
+###list 
+#col=["rsm","ram","you","hry"]
+#col.append("charchil")
+#col.insert(2,"SIDHU")
+#col.reverse()
+#print(col)
+#print(type(col))
+###
+#
+###while loop
+#
+#i= 0
+#while i<5:
+#    if i == 3:
+#        break
+#    print(i)
+#    i += 1
+# ###
+#
+#
+####
+#import random
+#print("welcom to the rps gsme")
+#uC=input("enter a either rock or paper or scissors:")
+#cI=["rock","paper","scissors"]
+#cC=random.choice(cI)
+#print(cC)
+
+
+
+
+
+
+
+#S="Python has a simple syntax"
+#
+#a=str(input("a "))
+#b=str(input("b "))
+#c=str(input("c "))
+#sp = str.split()
+#if a in sp:
+#    print(a)("True")
+#else:
+#    print(a)("False")
+#if b in sp:
+#    print(b)("True")
+#else:
+#    print(b)("False")
+#if c in sp:
+#    print(c)("True")
+#else:
+#    print(c)("False")
+
+
+    ###
+    
+    
+    
+#a=4
+#b=5
+#if a is b:
+#    print("True")
+#else:
+#    print("5 is 4 = False")
+
+##
+#p=21
+#q=33
+#c=(p<q)
+#b=(p<=q)
+#print(p)
+#print(q)
+#if(p<q and p<=q):
+#    print("true")
+#else:
+#    print("false")  
+#
+#if(p<q is not p<=q):
+#    print("false") 
+#else:
+#    print("true")       
+         
+##
+# Take input from user.
+#num = int(input("Print sum of even numbers till : "))
+#
+#total = 0
+#
+#for i in range(1, num + 1):
+#
+#    # Check for even or not.
+#    if((i % 2) == 0):
+#        total = total + i
+#
+#print("\nSum of even numbers from 1 to", num, "is :", total)
+##
+
+n=int(input("enter number:"))
+sum = (n*(n+1))/2
+hum = (n*(n+1))
+print(int(sum))
+print(hum)
+
+
+
+
+
+
